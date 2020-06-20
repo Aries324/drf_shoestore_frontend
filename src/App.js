54ef52608a1a033c7490c791127f2b61e64e7d95
Tiffany McLean <tamclean324@gmail.com>
2020-06-19 20:49:57 -0400

    creates a frontend reach app for the backend shoe api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {shoes:[]};
+  }
+  componentDidMount() {
+    fetch('http://localhost:8000/api/shoe')
+      .then(res => res.json())
+      .then(data =>this.setState({shoes:data}));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Shoe Paradise</h1>
+        <ul>
+          {this.state.shoes.map((s) => {
+            return (
+              <div style={{padding: 10}}>
+                <li>{s.size}</li>
+                <li>{s.brand_name}</li>
+                <li>{s.manufacturer}</li>
+                <li>{s.color}</li>
+                <li>{s.material}</li>
+                <li>{s.shoe_type}</li>
+                <li>{s.fasten_type}</li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
